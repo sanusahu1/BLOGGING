a6d1bd3de9f317077053d7f0b4fd8476a226cd18
Smurti <sahu.smruti@technoboot.co>
2024-01-06 23:52:58 +0530

    update

--- routes/user_router.js
+++ routes/user_router.js
@@ -1,6 +1,19 @@
 const { Router } = require("express");
 const router = Router();
 const User = require('../models/user_model');
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve("./public/images"));
+    },
+    filename: function (req, file, cb) {
+        const fileName = `${Date.now()}-${file.originalname}`;
+        cb(null, fileName);
+    },
+});
+const upload = multer({ storage: storage });
 
 router.get('/signin', (req, res) => {
     return res.render("signin");
@@ -10,12 +23,13 @@ router.get('/signup', (req, res) => {
     return res.render("signup");
 });
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', upload.single("profilePicUrl"), async (req, res) => {
     const { name, email, password } = req.body;
     await User.create({
         name,
         email,
         password,
+        profilePicUrl: `${req.file.filename}`
     });
     return res.redirect("/");
 });
